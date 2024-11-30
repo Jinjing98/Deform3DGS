afc813d82bf424e718eea6b8b562d19fa38c62fd
jinjing98 <xjj892290531@163.com>
2024-11-30 23:59:12 +0100

    extend utils by copying func from street

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -15,6 +15,7 @@ from datetime import datetime
 import numpy as np
 import random
 import cv2
+import torch.nn.functional as F
 
 def inpaint_rgb(rgb_image, mask):
     # Convert mask to uint8
@@ -149,3 +150,143 @@ def safe_state(silent):
     np.random.seed(0)
     torch.manual_seed(0)
     torch.cuda.set_device(torch.device("cuda:0"))
+
+
+
+#cp from stree gs
+
+def _sqrt_positive_part(x: torch.Tensor) -> torch.Tensor:
+    """
+    Returns torch.sqrt(torch.max(0, x))
+    but with a zero subgradient where x is 0.
+    """
+    ret = torch.zeros_like(x)
+    positive_mask = x > 0
+    ret[positive_mask] = torch.sqrt(x[positive_mask])
+    return ret
+
+def quaternion_to_matrix_numpy(r):
+    q = r / np.linalg.norm(r)
+
+    R = np.zeros((3, 3))
+
+    r = q[0]
+    x = q[1]
+    y = q[2]
+    z = q[3]
+
+    R[0, 0] = 1 - 2 * (y*y + z*z)
+    R[0, 1] = 2 * (x*y - r*z)
+    R[0, 2] = 2 * (x*z + r*y)
+    R[1, 0] = 2 * (x*y + r*z)
+    R[1, 1] = 1 - 2 * (x*x + z*z)
+    R[1, 2] = 2 * (y*z - r*x)
+    R[2, 0] = 2 * (x*z - r*y)
+    R[2, 1] = 2 * (y*z + r*x)
+    R[2, 2] = 1 - 2 * (x*x + y*y)
+    return R
+
+def quaternion_to_matrix(r):
+    norm = torch.sqrt(r[:,0]*r[:,0] + r[:,1]*r[:,1] + r[:,2]*r[:,2] + r[:,3]*r[:,3])
+
+    q = r / norm[:, None]
+
+    R = torch.zeros((q.size(0), 3, 3), device='cuda')
+
+    r = q[:, 0]
+    x = q[:, 1]
+    y = q[:, 2]
+    z = q[:, 3]
+
+    R[:, 0, 0] = 1 - 2 * (y*y + z*z)
+    R[:, 0, 1] = 2 * (x*y - r*z)
+    R[:, 0, 2] = 2 * (x*z + r*y)
+    R[:, 1, 0] = 2 * (x*y + r*z)
+    R[:, 1, 1] = 1 - 2 * (x*x + z*z)
+    R[:, 1, 2] = 2 * (y*z - r*x)
+    R[:, 2, 0] = 2 * (x*z - r*y)
+    R[:, 2, 1] = 2 * (y*z + r*x)
+    R[:, 2, 2] = 1 - 2 * (x*x + y*y)
+    return R
+
+def matrix_to_quaternion(matrix: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as rotation matrices to quaternions.
+
+    Args:
+        matrix: Rotation matrices as tensor of shape (..., 3, 3).
+
+    Returns:
+        quaternions with real part first, as tensor of shape (..., 4).
+    """
+    if matrix.size(-1) != 3 or matrix.size(-2) != 3:
+        raise ValueError(f"Invalid rotation matrix shape {matrix.shape}.")
+
+    batch_dim = matrix.shape[:-2]
+    m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.unbind(
+        matrix.reshape(batch_dim + (9,)), dim=-1
+    )
+
+    q_abs = _sqrt_positive_part(
+        torch.stack(
+            [
+                1.0 + m00 + m11 + m22,
+                1.0 + m00 - m11 - m22,
+                1.0 - m00 + m11 - m22,
+                1.0 - m00 - m11 + m22,
+            ],
+            dim=-1,
+        )
+    )
+
+    # we produce the desired quaternion multiplied by each of r, i, j, k
+    quat_by_rijk = torch.stack(
+        [
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([q_abs[..., 0] ** 2, m21 - m12, m02 - m20, m10 - m01], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m21 - m12, q_abs[..., 1] ** 2, m10 + m01, m02 + m20], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m02 - m20, m10 + m01, q_abs[..., 2] ** 2, m12 + m21], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m10 - m01, m20 + m02, m21 + m12, q_abs[..., 3] ** 2], dim=-1),
+        ],
+        dim=-2,
+    )
+
+    # We floor here at 0.1 but the exact level is not important; if q_abs is small,
+    # the candidate won't be picked.
+    flr = torch.tensor(0.1).to(dtype=q_abs.dtype, device=q_abs.device)
+    quat_candidates = quat_by_rijk / (2.0 * q_abs[..., None].max(flr))
+
+    # if not for numerical problems, quat_candidates[i] should be same (up to a sign),
+    # forall i; we pick the best-conditioned one (with the largest denominator)
+
+    return quat_candidates[
+        F.one_hot(q_abs.argmax(dim=-1), num_classes=4) > 0.5, :
+    ].reshape(batch_dim + (4,))
+  
+
+
+def startswith_any(k, l):
+    for s in l:
+        if k.startswith(s):
+            return True
+    return False
+
+    
+def NumpytoTorch(image, resolution, resize_mode=cv2.INTER_AREA):
+    if resolution is not None:
+        image = cv2.resize(image, resolution, interpolation=resize_mode)
+    
+    image = torch.from_numpy(np.array(image))
+    if len(image.shape) == 2:
+        image = image[..., None].permute(2, 0, 1) # [1, H, W]
+    elif len(image.shape) == 3:
+        image = image.permute(2, 0, 1)
+    
+    return image
