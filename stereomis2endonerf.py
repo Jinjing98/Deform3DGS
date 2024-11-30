16464ed89d998fce5afb72a00202cc95b9aa43b6
jinjing98 <xjj892290531@163.com>
2024-11-30 16:05:27 +0100

    use raft fill the missin g depth on cluster endonerf dataset

--- stereomis2endonerf.py
+++ stereomis2endonerf.py
@@ -64,6 +64,100 @@ class DepthEstimator(torch.nn.Module):
         depth[~valid] = 0.0
         return depth.unsqueeze(1)
         
+
+def fake_endonerf_complete_depth(data_dir, intri_dir, start_frame, end_frame, \
+                                 img_size=(512, 640), dst_dir_base = None, skip_depth_infer = False,
+                                 ):
+    """
+    use SM intri due to lack of intri
+    """
+    if dst_dir_base == None:
+        assert 0, "better write the processed data to another dir"
+    else:
+        #udpate
+        assert 'P' in intri_dir,intri_dir
+        intri_dir = os.path.join(STEREOMIS_BASE,intri_dir)
+        dst_dir = dst_dir_base #os.path.join(dst_dir_base,os.path.basename(intri_dir))
+        os.makedirs(dst_dir, exist_ok=True)
+
+    # Load parameters after rectification
+    calib_file = os.path.join(intri_dir, 'StereoCalibration.ini')
+    assert os.path.exists(calib_file), "Calibration file not found."
+    rect = StereoRectifier(calib_file, img_size_new=(img_size[1], img_size[0]), mode='conventional')
+    calib = rect.get_rectified_calib()
+    baseline = calib['bf'].astype(np.float32)
+    intrinsics = calib['intrinsics']['left'].astype(np.float32)
+    
+    # Sort images and masks according to the start and end frame indexes
+    data_dir = os.path.join(ENDONERF_BASE,data_dir)
+    frames = sorted(os.listdir(os.path.join(data_dir, 'images')))
+    # assert 0, frames
+    # frames = [f for f in frames if 'l.png' in f and int(f.split('l.')[0]) >= start_frame and int(f.split('l.')[0]) <= end_frame]
+    assert len(frames) > 0, "No frames found."
+    resize = Resize(img_size)
+    resize_msk = Resize(img_size, interpolation=InterpolationMode.NEAREST)
+    
+    if not skip_depth_infer:
+        # Configurate depth estimator. We follow the settings of RAFT in robust-pose-estimator(https://github.com/aimi-lab/robust-pose-estimator)
+        depth_estimator = DepthEstimator(RAFT_config)
+    else:
+        print('Read depth from local.')
+    # Create folders
+    # output_dir = os.path.join(data_dir, 'stereo_'+ os.path.basename(data_dir)+'_'+str(start_frame)+'_'+str(end_frame))
+    output_dir = os.path.join(dst_dir, os.path.basename(data_dir)+'_'+str(start_frame)+'_'+str(end_frame))
+    # image_dir = os.path.join(output_dir, 'images')
+    # mask_dir = os.path.join(output_dir, 'masks')
+    depth_dir = os.path.join(output_dir, 'depth')
+    # if not os.path.exists(image_dir):
+    #     os.makedirs(image_dir)
+    # if not os.path.exists(mask_dir):
+    #     os.makedirs(mask_dir)
+    if not os.path.exists(depth_dir):
+        os.makedirs(depth_dir)
+    poses_bounds = []
+    from tqdm import tqdm
+    print('!*******************************************')
+    print('!!!!Hard set 0 pose----only static cam!')
+    for i, frame in enumerate(tqdm(frames)):
+        left_img = torch.from_numpy(cv2.cvtColor(cv2.imread(os.path.join(data_dir, 'images', frame)), cv2.COLOR_BGR2RGB)).permute(2, 0, 1).float()
+        right_img = torch.from_numpy(cv2.cvtColor(cv2.imread(os.path.join(data_dir, 'images_right', frame.replace('frame-', '').replace('.color', ''))), cv2.COLOR_BGR2RGB)).permute(2, 0, 1).float()
+        left_img = resize(left_img)
+        right_img = resize(right_img)
+        if skip_depth_infer:
+            assert 0,'not properly saved as actual depth values....check out the slam generated /flow_depth, might be proper...'
+        else:
+            with torch.no_grad():
+                depth = depth_estimator(left_img[None], right_img[None], baseline[None])
+        try:
+            mask = read_mask(os.path.join(data_dir, 'masks', frame))
+            mask = resize_msk(mask)
+        except:
+            mask = torch.ones(1, img_size[0], img_size[1])
+            
+        # Save the data. Of note, the file should start with 'stereo_' to be compatible with the dataloader in Deform3DGS.
+        left_img_np = left_img.permute(1, 2, 0).numpy()
+        mask_np = mask.permute(1, 2, 0).numpy()
+        left_img_bgr = cv2.cvtColor(left_img_np, cv2.COLOR_RGB2BGR)
+ 
+        # Save left_img, right_img, and mask to output_dir
+        name = 'frame-'+str(i).zfill(6)+'.color.png'
+        # cv2.imwrite(os.path.join(image_dir, name), left_img_bgr)
+        # cv2.imwrite(os.path.join(mask_dir, name.replace('color','mask')), mask_np.astype(np.uint8) * 255)
+        cv2.imwrite(os.path.join(depth_dir, name.replace('color','depth')), depth[0, 0].cpu().numpy())
+
+        # Save poses_bounds.npy. Only static view is considered, i.e., R = I and T = 0.
+        R = np.eye(3)
+        T = np.zeros(3)
+        extr = np.concatenate([R, T[:, None]], axis=1)
+        cy, cx, focal = intrinsics[1, 2], intrinsics[0, 2], intrinsics[0, 0]
+        param = np.concatenate([extr, np.array([[cy, cx, focal]]).T], axis=1)    
+        param = param.reshape(1, 15)
+        param = np.concatenate([param, np.array([[0.03, 250.]])], axis=1)
+        poses_bounds.append(param[0])
+        
+    np.save(os.path.join(output_dir, 'poses_bounds.npy'), np.array(poses_bounds))
+ 
+
 def reformat_dataset(data_dir, start_frame, end_frame, img_size=(512, 640), dst_dir_base = None, skip_depth_infer = False):
     """
     Reformat the StereoMIS to the same format as EndoNeRF dataset by stereo depth estimation.
@@ -153,7 +247,8 @@ def reformat_dataset(data_dir, start_frame, end_frame, img_size=(512, 640), dst_
     np.save(os.path.join(output_dir, 'poses_bounds.npy'), np.array(poses_bounds))
     
 if __name__ == "__main__":            
-    STEREOMIS_BASE = '/mnt/ceph/tco/TCO-All/SharedDatasets/StereoMIS/'
+    ENDONERF_BASE = '/mnt/cluster/datasets/EndoNeRF'
+    STEREOMIS_BASE = '/mnt/ceph/tco/TCO-All/SharedDatasets/StereoMIS'
     torch.manual_seed(1234)
     np.random.seed(1234)
     # Set up command line argument parser
@@ -165,3 +260,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     frame_id = args.frame_id
     reformat_dataset(args.data_dir,frame_id[0], frame_id[1],dst_dir_base=args.dst_dir_base, skip_depth_infer = False)
+
+    # data_dir = 'pulling'
+    # data_dir = 'cutting'
+    # fake_endonerf_complete_depth(data_dir=data_dir,
+    #                              intri_dir = 'P2_3',\
+    #                                 start_frame = frame_id[0], \
+    #                                     end_frame=frame_id[1],\
+    #                                     dst_dir_base=f'{ENDONERF_BASE}/processed', \
+    #                                         skip_depth_infer = False)
