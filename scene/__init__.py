abced729146f5805973586d116dce74744c4b703
jinjing98 <xjj892290531@163.com>
2024-12-05 09:04:06 +0100

    scene extend to be used with misgs: break down init to 1)dataloading(callback--will prepare the init_pcd too)--including meta used for misgs 2) gaussians actual init with the "pcd"

--- scene/__init__.py
+++ scene/__init__.py
@@ -16,51 +16,45 @@ from utils.system_utils import searchForMaxIteration
 from scene.dataset_readers import sceneLoadTypeCallbacks
 from scene.flexible_deform_model import TissueGaussianModel
 from arguments import ModelParams
-from utils.camera_utils import cameraList_from_camInfos, camera_to_JSON
-from torch.utils.data import Dataset
 
 from typing import Union
 # from gaussian_model_base import GaussianModelBase
 from scene.tool_movement_model import GaussianModelActor
 from scene.mis_gaussian_model import MisGaussianModel
+from config.argsgroup_misgs import ModParams
+
 class Scene:
 
     # gaussians : TissueGaussianModel
     # gaussians : Union[GaussianModelBase, MisGaussianModel]
-    gaussians : Union[TissueGaussianModel, GaussianModelActor, MisGaussianModel]
+    gaussians_or_controller : Union[TissueGaussianModel, GaussianModelActor, MisGaussianModel]
     
-    def __init__(self, args : ModelParams, \
-                 gaussians : Union[TissueGaussianModel, GaussianModelActor, MisGaussianModel], \
-                    load_iteration=None):
+    def __init__(self, \
+                #  args : ModelParams,
+                 args : Union[ModelParams,ModParams],#Dataparams used for load scene_meta
+                 load_other_obj_meta = False,
+                 new_cfg = None,
+                 ):
         """b
         :param path: Path to colmap scene main folder.
         """
         self.model_path = args.model_path
-        self.loaded_iter = None
-        self.gaussians = gaussians
-        
-        #extend
-        # print('TODO','remove, not necessary?')
-        # self.tool_mask = args.tool_mask
-        
-        if load_iteration:
-            if load_iteration == -1:
-                self.loaded_iter = searchForMaxIteration(os.path.join(self.model_path, "point_cloud"))
-            else:
-                self.loaded_iter = load_iteration
-            print("Loading trained model at iteration {}".format(self.loaded_iter))
-        
         if os.path.exists(os.path.join(args.source_path, "poses_bounds.npy")) and args.extra_mark == 'endonerf':
             scene_info = sceneLoadTypeCallbacks["endonerf"](args.source_path,
                                                             tool_mask=args.tool_mask,
                                                             init_mode=args.init_mode,
+                                                            load_other_obj_meta=load_other_obj_meta,
+                                                            cfg = new_cfg,
+                                                            
                                                             )
             print("Found poses_bounds.py and extra marks with EndoNeRf")
         elif os.path.exists(os.path.join(args.source_path, "point_cloud.obj")) or os.path.exists(os.path.join(args.source_path, "left_point_cloud.obj")):
+            assert 0,'not for misgs yet'
             assert self.tool_mask == 'use', NotImplementedError
             scene_info = sceneLoadTypeCallbacks["scared"](args.source_path, args.white_background, args.eval)
             print("Found point_cloud.obj, assuming SCARED data!")
         else:
+            assert 0,'not for misgs yet'
             assert False, "Could not recognize scene type!"
                 
         self.maxtime = scene_info.maxtime
@@ -78,27 +72,49 @@ class Scene:
         # assert 0, f'{scene_info.point_cloud.points} {len(scene_info.point_cloud.points)}'
         xyz_max = scene_info.point_cloud.points.max(axis=0)
         xyz_min = scene_info.point_cloud.points.min(axis=0)
-        # self.gaussians._deformation.deformation_net.grid.set_aabb(xyz_max,xyz_min)
+        # self.gaussians_or_controller._deformation.deformation_net.grid.set_aabb(xyz_max,xyz_min)
+        #jj
+        print("Loading point_cloud (use for can step-step train init)")
+        self.point_cloud =  scene_info.point_cloud 
+        self.scene_metadata = scene_info.scene_metadata
 
+        self.loaded_iter = None
+        self.gaussians_or_controller = None
+
+
+
+    def gs_init(self,gaussians_or_controller : Union[TissueGaussianModel, GaussianModelActor, MisGaussianModel],\
+                load_iteration=None,
+                reset_camera_extent = None,
+                ):
+        # self.loaded_iter = None
+        self.gaussians_or_controller = gaussians_or_controller
+        if load_iteration:
+            if load_iteration == -1:
+                self.loaded_iter = searchForMaxIteration(os.path.join(self.model_path, "point_cloud"))
+            else:
+                self.loaded_iter = load_iteration
+            print("Loading trained model at iteration {}".format(self.loaded_iter))
         if self.loaded_iter:
-            self.gaussians.load_ply(os.path.join(self.model_path,
+            self.gaussians_or_controller.load_ply(os.path.join(self.model_path,
                                                            "point_cloud",
                                                            "iteration_" + str(self.loaded_iter),
                                                            "point_cloud.ply"))
-            self.gaussians.load_model(os.path.join(self.model_path,
+            self.gaussians_or_controller.load_model(os.path.join(self.model_path,
                                                     "point_cloud",
                                                     "iteration_" + str(self.loaded_iter),
                                                    ))
         else:
-            self.gaussians.create_from_pcd(scene_info.point_cloud, args.camera_extent, self.maxtime)
+            # depending on the spercific init_strategy: it will update self.point_cloud
+            self.gaussians_or_controller.create_from_pcd(self.point_cloud, reset_camera_extent, self.maxtime)
 
     def save(self, iteration, stage):
         if stage == "coarse":
             point_cloud_path = os.path.join(self.model_path, "point_cloud/coarse_iteration_{}".format(iteration))
         else:
             point_cloud_path = os.path.join(self.model_path, "point_cloud/iteration_{}".format(iteration))
-        self.gaussians.save_ply(os.path.join(point_cloud_path, "point_cloud.ply"))
-        # self.gaussians.save_deformation(point_cloud_path)
+        self.gaussians_or_controller.save_ply(os.path.join(point_cloud_path, "point_cloud.ply"))
+        # self.gaussians_or_controller.save_deformation(point_cloud_path)
     
     def getTrainCameras(self, scale=1.0):
         return self.train_camera
@@ -108,4 +124,7 @@ class Scene:
 
     def getVideoCameras(self, scale=1.0):
         return self.video_camera
+    
+    def getSceneMetaData(self):
+        return self.scene_metadata
     
