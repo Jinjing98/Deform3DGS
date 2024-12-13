d05f2b292e08581b27ec78328738d6b0d6f5adf6
jinjing98 <xjj892290531@163.com>
2024-12-13 12:10:31 +0100

    scene_info: support load pcd as dict piecewise for misgs

--- scene/__init__.py
+++ scene/__init__.py
@@ -15,6 +15,7 @@ import json
 from utils.system_utils import searchForMaxIteration
 from scene.dataset_readers import sceneLoadTypeCallbacks
 from scene.flexible_deform_model import TissueGaussianModel
+from scene.tool_model import ToolModel
 from arguments import ModelParams
 
 from typing import Union
@@ -22,6 +23,8 @@ from typing import Union
 from scene.tool_movement_model import GaussianModelActor
 from scene.mis_gaussian_model import MisGaussianModel
 from config.argsgroup_misgs import ModParams
+import torch.nn as nn
+
 
 class Scene:
 
@@ -34,10 +37,13 @@ class Scene:
                  args : Union[ModelParams,ModParams],#Dataparams used for load scene_meta
                  load_other_obj_meta = False,
                  new_cfg = None,
+                 load_pcd_dict_in_sceneinfo = False,
                  ):
         """b
         :param path: Path to colmap scene main folder.
         """
+        self.load_pcd_dict_in_sceneinfo = load_pcd_dict_in_sceneinfo
+
         self.model_path = args.model_path
         if os.path.exists(os.path.join(args.source_path, "poses_bounds.npy")) and args.extra_mark == 'endonerf':
             scene_info = sceneLoadTypeCallbacks["endonerf"](args.source_path,
@@ -45,6 +51,7 @@ class Scene:
                                                             init_mode=args.init_mode,
                                                             load_other_obj_meta=load_other_obj_meta,
                                                             cfg = new_cfg,
+                                                            load_pcd_dict_in_sceneinfo=self.load_pcd_dict_in_sceneinfo,
                                                             
                                                             )
             print("Found poses_bounds.py and extra marks with EndoNeRf")
@@ -70,12 +77,14 @@ class Scene:
         self.video_camera =  scene_info.video_cameras 
         
         # assert 0, f'{scene_info.point_cloud.points} {len(scene_info.point_cloud.points)}'
-        xyz_max = scene_info.point_cloud.points.max(axis=0)
-        xyz_min = scene_info.point_cloud.points.min(axis=0)
+        # xyz_max = scene_info.point_cloud.points.max(axis=0)
+        # xyz_min = scene_info.point_cloud.points.min(axis=0)
         # self.gaussians_or_controller._deformation.deformation_net.grid.set_aabb(xyz_max,xyz_min)
         #jj
         print("Loading point_cloud (use for can step-step train init)")
         self.point_cloud =  scene_info.point_cloud 
+        self.point_cloud_dict =  scene_info.point_cloud_dict 
+
         self.scene_metadata = scene_info.scene_metadata
 
         self.loaded_iter = None
@@ -105,8 +114,20 @@ class Scene:
                                                     "iteration_" + str(self.loaded_iter),
                                                    ))
         else:
-            # depending on the spercific init_strategy: it will update self.point_cloud
-            self.gaussians_or_controller.create_from_pcd(self.point_cloud, reset_camera_extent, self.maxtime)
+            if self.load_pcd_dict_in_sceneinfo:
+                # depending on the spercific init_strategy: it will update self.point_cloud
+                assert self.point_cloud_dict!={}
+                assert isinstance(self.gaussians_or_controller,nn.Module)
+                self.gaussians_or_controller.create_from_pcd(pcd_dict=self.point_cloud_dict, 
+                                                             spatial_lr_scale=reset_camera_extent, 
+                                                             time_line=self.maxtime)
+            else:
+                assert self.point_cloud!=None
+                assert isinstance(self.gaussians_or_controller, Union[TissueGaussianModel,ToolModel])
+                 #gs model
+                self.gaussians_or_controller.create_from_pcd(pcd=self.point_cloud, 
+                                                             spatial_lr_scale=reset_camera_extent, 
+                                                             time_line=self.maxtime)
 
     def save(self, iteration, stage):
         if stage == "coarse":
